393ced284181563d4e8cd024e15a1e4ce1dbfc79
Linus Alpsten <linus.alpsten@telia.com>
2019-09-02 15:50:02 +0200

    dump

---
+++ Mat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAdventure
+{
+    public class Food
+    {
+        public int scoreModifier = 0;
+        public string name = "";
+        public int Eat(int score)
+        {
+            return score + scoreModifier;
+        }
+    }
+
+    public class Mouse : Food
+    {
+        public Mouse()
+        {
+            scoreModifier = 3;
+            name = "Mus";
+        }
+    }
+
+    public class Rat : Food
+    {
+        public Rat()
+        {
+            scoreModifier = 5;
+            name = "Råtta";
+        }
+    }
+
+    public class Bird : Food
+    {
+        public Bird()
+        {
+            scoreModifier = 4;
+            name = "Fågel";
+        }
+    }
+
+    public class Berry : Food
+    {
+        public Berry()
+        {
+            scoreModifier = -3;
+            name = "Bär";
+        }
+    }
+
+    public class Apple : Food
+    {
+        public Apple()
+        {
+            scoreModifier = -5;
+            name = "Äpple";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,79 @@ namespace SnakeAdventure
 {
     class Program
     {
+        const string name = "Oscar";
+        static Locations location = Locations.Skogen;
+        static int score = 0;
+        List<Food> inventory = new List<Food>();
+        static Random rng = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            //bool gameover = false;
+            ////Introduction
+            //Console.WriteLine($"Du ska hjälpa ormen {name} att hitta mat");
+            //Console.WriteLine("Du behöver 20 poäng för att gå vidare till nästa nivå");
+            //while (!gameover)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine($"Poäng: {score}");
+            //    Console.WriteLine($"Du befinner dig i {location}");
+            //    Console.WriteLine("1. Leta mat");
+            //    Console.WriteLine("2. Ryggsäck");
+            //    string input = Console.ReadLine();
+            //    switch (input)
+            //    {
+            //        case "1":
+
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //}
+            FindFood();
+        }
+
+        public static void FindFood()
+        {
+            Food food;
+            switch (rng.Next(5))
+            {
+                case 0:
+                    food = new Mouse();
+                    break;
+                case 1:
+                    food = new Rat();
+                    break;
+                case 2:
+                    food = new Bird();
+                    break;
+                case 3:
+                    food = new Berry();
+                    break;
+                case 4:
+                    food = new Apple();
+                    break;
+                default:
+                    throw new Exception("Could not generate food");
+                    break;
+            }
+            if (food.GetType() == typeof(Berry)||food.GetType() == typeof(Apple))
+            {
+                Console.WriteLine($"Du hittade ett {food.name}.");
+            }
+            else
+            {
+                Console.WriteLine($"Du hittade en {food.name}");
+            }
+            char input;
+            do
+            {
+                Console.WriteLine("Vill du spara det du hittade?");
+                Console.WriteLine("1. Ja");
+                Console.WriteLine("2. Nej");
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine(input);
+
+            } while (input != '1' && input != '2');
         }
     }
 }
